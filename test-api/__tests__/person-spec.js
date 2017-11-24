7ca41a818465bf0b6fde584025e45925de61a706
markc <markc@pageuppeople.com>
2017-11-25 10:07:21 +1100

    Get basic tests working again

--- test-api/__tests__/person-spec.js
+++ test-api/__tests__/person-spec.js
@@ -1,27 +1,27 @@
 const frisby = require('frisby');
 const uuidV4 = require('uuid/v4');
 
-it('POST /api/person with a valid body returns 201(created)', function(done) {
+describe("/api/person", () => {
+    it('POST /api/person with a valid body returns 201(created)', function(done) {
+        const personId = uuidV4();
+        frisby.post('http://localhost:8080/api/person',
+            {
+                "id" : personId,
+                "name": "john smith"
+            })
+            .expect('status', 200)
+            .done(done);
+    });
 
-    const personId = uuidV4();
-    frisby.post('http://localhost:8080/api/person',
-        {
-            "id" : personId,
-            "name": "john smith"
-        })
-        .expect('status', 200)
-        .done(done);
-});
+    it('GET /api/person retrieves person', function(done) {
 
-
-it('GET /api/person retrieves person', function(done) {
-
-    const personId = uuidV4();
-    frisby.post('http://localhost:8080/api/person',
-        {
-            "id" : personId,
-            "name": "john smith"
-        })
-        .expect('status', 200)
-        .done(done);
+        const personId = uuidV4();
+        frisby.post('http://localhost:8080/api/person',
+            {
+                "id" : personId,
+                "name": "john smith"
+            })
+            .expect('status', 200)
+            .done(done);
+    });
 });
